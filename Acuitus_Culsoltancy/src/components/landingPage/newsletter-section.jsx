a667fdfec8261c4d035c999934385d6b66f0cdb4
erickadikah <erickadikah2030@gmail.com>
2023-08-23 03:02:02 +0300

    update ui

--- Acuitus_Culsoltancy/src/components/landingPage/newsletter-section.jsx
+++ Acuitus_Culsoltancy/src/components/landingPage/newsletter-section.jsx
@@ -8,159 +8,147 @@ export default function NewsLetterSection() {
     background: `rgba(35, 35, 35, 0.9)`,
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    padding: "100px 10px",
     display: "flex",
     placeContent: "center",
-    height: "65rem",
+    height: "80%",
     alignItems: "center",
     justifyContent: "center",
-    gap: "50px",
     flexDirection: "row",
   };
 
   const buttonStyles = {
-    background: "#145038",
     height: "50px",
     width: "150px",
     borderRadius: "40px",
     border: "none",
     fontSize: "16px",
     fontWeight: "bolder",
+    color: 'white',
     transition: "background 0.3s, color 0.3s", // Smooth transition for both background and color
   };
 
   const handleMouseEnter = (event) => {
-    event.target.style.background = "white";
-    event.target.style.color = "#145038";
+    event.target.style.background = "";
+    event.target.style.color = "";
   };
 
   const handleMouseLeave = (event) => {
     event.target.style.background = "#145038";
-    event.target.style.color = "#145038";
+    event.target.style.color = "white";
   };
 
   return (
-    <section style={styles}>
-      <div
+    <section style={styles} className="form">
+    <div className="newsletter">
+    <div className="first-cont">
+      <h1
+        className="text-white"
         style={{
-          height: "400px",
-          width: "400px",
-          marginBottom: "20rem",
+          fontSize: "50px",
+          fontWeight: "bolder",
         }}
       >
-        <h1
-          className="text-white"
+        Our solutions for your
+        <span
           style={{
-            fontSize: "50px",
-            fontWeight: "bolder",
+            color: "#145038",
           }}
         >
-          Our solutions for your
-          <span
-            style={{
-              color: "#145038",
-            }}
-          >
-            {" "}
-            business growth
-          </span>
-        </h1>
-        <br></br>
-        <p
-          className="text-white"
-          style={{
-            fontSize: "18px",
-          }}
-        >
-          We pride ourselves in providing training and coaching that aims to
-          improve food security while providing sustainable livelihood options
-          for farmers especially the women and the youth in agribusiness. We,
-          therefore, establish sustainable partnerships that promote and share
-          our vision for the agribusiness sector. Our Agricultural Financial
-          Inclusion programs aim to provide small-holder farmers and MSMEs with
-          access to affordable financing through linkage to financial
-          institutions and also the adoption of the VSLA model.
-        </p>
-        <br></br>
-        <p
-          style={{
-            fontSize: "18px",
-          }}
-          className="text-white"
-        >
-          This has helped to establish community-based financing thus promoting
-          sustainable rural financial models and financial literacy, especially
-          for rural farmers.
-        </p>
-        <button
-          style={buttonStyles}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <a
-            style={{
-              textDecoration: "none",
-            }}
-          >
-            Read More
-          </a>
-        </button>
-      </div>
-      <div
+          {" "}
+          business growth
+        </span>
+      </h1>
+      <br></br>
+      <p
+        className="text-white"
         style={{
-          marginLeft: "50px",
-          marginTop: "14rem",
+          fontSize: "18px",
         }}
       >
+        We pride ourselves in providing training and coaching that aims to
+        improve food security while providing sustainable livelihood options
+        for farmers especially the women and the youth in agribusiness. We,
+        therefore, establish sustainable partnerships that promote and share
+        our vision for the agribusiness sector. Our Agricultural Financial
+        Inclusion programs aim to provide small-holder farmers and MSMEs with
+        access to affordable financing through linkage to financial
+        institutions and also the adoption of the VSLA model.
+      </p>
+      <br></br>
+      <p
+        style={{
+          fontSize: "18px",
+        }}
+        className="text-white"
+      >
+        This has helped to establish community-based financing thus promoting
+        sustainable rural financial models and financial literacy, especially
+        for rural farmers.
+      </p>
+      <button
+        style={buttonStyles}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <a
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          Read More
+        </a>
+      </button>
+    </div>
+    <div
+      style={{
+        marginLeft: "50px",
+        marginTop: "10rem",
+
+      }}
+    >
         <div
           style={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "30px",
-          }}
-        >
-          <div
-            style={{
-              display: "block",
-            }}
-          >
-            <img
-              src={heroImage4}
-              alt="image"
-              style={{ 
-              width: "600px", 
-              height: "400px" }}
-            />
-            <p
-              style={{
-                fontSize: "16px",
-                color: "black",
-                width: "600px",
-                height: "200px",
-                background: "#FFFFFF",
-                padding: "20px 10px",
-              }}
+            display: "block"}}
+            className="card-content"
             >
-              <ul className="arrow-list">
-                <li>
-                  <FaArrowRight className="arrow-icon" />
-                  Acuitus Duo Company Ltd provides Market research and analysis
-                  that offer clients actionable solutions to their problems.
-                </li>
+          <img
+            src={heroImage4}
+            alt="image"
+            style={{ 
+            width: "600px", 
+            height: "400px" }}
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              color: "black",
+              width: "600px",
+              height: "200px",
+              background: "#FFFFFF",
+              padding: "20px 10px",
+            }}
+            className="card-text"
+          >
+            <ul className="arrow-list">
+              <li>
                 <FaArrowRight className="arrow-icon" />
-                The world is rapidly changing therefore the need for reliable
-                information to make confident decisions has never been greater.
-                <li>
-                  <FaArrowRight className="arrow-icon" />
-                  At Acuitus we believe our clients need more than a data
-                  supplier, they need a partner who can produce accurate and
-                  relevant information and turn it into actionable truth.
-                </li>
-              </ul>
-            </p>
-          </div>
+                Acuitus Duo Company Ltd provides Market research and analysis
+                that offer clients actionable solutions to their problems.
+              </li>
+              <FaArrowRight className="arrow-icon" />
+              The world is rapidly changing therefore the need for reliable
+              information to make confident decisions has never been greater.
+              <li>
+                <FaArrowRight className="arrow-icon" />
+                At Acuitus we believe our clients need more than a data
+                supplier, they need a partner who can produce accurate and
+                relevant information and turn it into actionable truth.
+              </li>
+            </ul>
+          </p>
         </div>
       </div>
+    </div>
     </section>
   );
 }
--- Acuitus_Culsoltancy/src/components/landingPage/service.jsx
+++ Acuitus_Culsoltancy/src/components/landingPage/service.jsx
@@ -1,21 +1,113 @@
-// OurServices.jsx
 import React from 'react';
-import { servicesCard } from './servicesDescriptor'; // Import your services data
-import ServicesAccordion from './services-accordion'; // Correct the import name
+import { FaArrowRight } from 'react-icons/fa'; // Import FaArrowRight
+import { servicesCard } from './servicesDescriptor';
+import ServicesAccordion from './services-accordion';
+import "../../assets/styles/landingPage/service.scss";
 
 export default function OurServices() {
   return (
     <div className='bg-white' style={{
       height: '100%',
       width: '100%',
-
     }}>
-    <div className='container' style={{
-      padding: "20px 10px",
-    }}>
-      <h1 className='text-center text-info'>Our Services</h1>
-      <ServicesAccordion servicesCard={servicesCard} /> {/* Correct the component name */}
-    </div>
+    <div className='company'>
+        <div className='about-company' style={{
+          padding: "20px 10px"
+        }}>
+          <h1 className='about-company__title text-center' style={{
+            color: '#145038',
+          }}>Welcome to <span style={{
+            color: '#8D2A1C',
+          }}> Acuitus Duo Consultancy Firm.</span></h1>
+          <div className='about' style={{
+            textAlign: 'center',
+            marginTop: '20px',
+          }}>
+            <h3 style={{ fontWeight: '600', color: '#145038' }}>About</h3>
+            <p style={{
+              fontSize: '18px',
+              color: '#333',
+              lineHeight: '1.6'
+            }}>
+              Acuitus Duo Co. Ltd is a global institutional capacity building, technical,
+              and management consultancy firm. We offer a diverse range of services including
+              education and training for individuals, groups, and organizations, sustainable
+              capacity-building programs, data analytics, brand/product research, market insights,
+              and sales and marketing strategies.
+            </p>
+            <button className='btn-r'>
+            Read More
+            </button>
+          </div>
+          <div>
+          <h3 style={{ marginTop: '20px',
+           fontWeight: '600',
+            color: '#145038',
+           }} className='text-center'>Vision</h3>
+          <p className='text-center' style={{
+            fontSize: '1.0rem',
+          }}>
+            To perpetuate life long skills in a way that expands your creative thinking, skills and stimulates your problem-solving abilities inorder to unlock more opportunities
+          </p>
+          </div>
+          <div>
+          <h3 style={{ marginTop: '20px', fontWeight: '600', color: '#145038' }} className='text-center'>Mission</h3>
+          <ul className='text-center' style={{
+            listStyle: 'none',
+            fontSize: '1.0rem',
+          }}>
+            <li>
+              <FaArrowRight className="arrow-icon" />
+              Develop and strengthen the skills, abilities, and processes required by communities and organizations to adapt, thrive, and survive in the modern world.
+            </li>
+            <li>
+              <FaArrowRight className="arrow-icon" />
+              Provide a platform for individuals, businesses and organizations to learn, grow and develop.
+            </li>
+            <li>
+              <FaArrowRight className="arrow-icon" />
+              Provide a platform for individuals, businesses and organizations to learn, grow and develop.
+            </li>
+          </ul>
+          </div>
+          <div className='about-company' style={{
+          padding: "20px 10px"
+        }}>
+          <h3 style={{ marginTop: '20px', fontWeight: '600', color: '#145038' }} className='text-center'>Core Values</h3>
+          <ul className='core-values-list text-center' style={{
+            display: 'flex', 
+            justifyContent: 'space-around',
+            padding: 0,
+            flexWrap: 'wrap',
+            fontSize: '1.0rem',
+          }}>
+            <li>
+              Integrity
+            </li>
+            <li>
+              Excellence
+            </li>
+            <li>
+              Innovation
+            </li>
+            <li>
+              Teamwork
+            </li>
+            <li>
+              Accountability
+            </li>
+          </ul>
+        </div>
+        </div>
+      </div>
+      <div className='container' style={{
+        padding: "20px 10px",
+        marginTop: '12rem',
+      }}>
+        <h1 className='text-center text-info'>Our Services</h1>
+        <ServicesAccordion servicesCard={servicesCard} />
+      </div>
     </div>
   );
 }
+
