4d7f4de6824ab668f0fa302e16ddc35ac587f1eb
erickadikah <erickadikah2030@gmail.com>
2023-09-02 13:05:30 +0300

    updated partners

--- Acuitus_Culsoltancy/src/components/landingPage/partner.jsx
+++ Acuitus_Culsoltancy/src/components/landingPage/partner.jsx
@@ -3,7 +3,9 @@ import "../../assets/styles/landingPage/partner.scss";
 import partner1 from '../../assets/images/landingPage/Colgate-Logo.png';
 import partner2 from '../../assets/images/landingPage/Arifu-Logo.png';
 import partner3 from '../../assets/images/landingPage/intelliwealth logo 2.png';
-import partner4 from '../../assets/images/landingPage/Worldv.png'
+import partner4 from '../../assets/images/landingPage/Worldv.png';
+import partner5 from '../../assets/images/landingPage/wfp.svg';
+import partner6 from '../../assets/images/landingPage/BigBad.webp';
 
 const Partner = () => {
   return (
@@ -22,7 +24,10 @@ const Partner = () => {
         <img src={partner2} alt='partner2' className='partner-image'/>
         <img src={partner3} alt='partner3' className='partner-image'/>
         <img src={partner4} alt='partner4' className='partner-image'/>
-        {/*<img src={require('../../assets/images/landingPage/partner5.png')} alt='partner5' />*/}
+        <img src={partner5} alt='partner5' className='partner-image' style={{
+          backgroundColor: `rgb(0,125,188)`
+        }}/>
+        <img src={partner6} alt='partner4' className='partner-image'/>
     </div>
     </div>
     </>
