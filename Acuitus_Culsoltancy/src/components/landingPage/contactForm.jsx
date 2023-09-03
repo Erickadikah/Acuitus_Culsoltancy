c0941b358721278e41530d46df70b9e2b9de326c
erickadikah <erickadikah2030@gmail.com>
2023-09-03 15:03:18 +0300

    update service page

--- Acuitus_Culsoltancy/src/components/landingPage/contactForm.jsx
+++ Acuitus_Culsoltancy/src/components/landingPage/contactForm.jsx
@@ -17,63 +17,60 @@ export default function Contactform() {
   });
 
   return (
-    <form onSubmit={form.onSubmit(() => {})}>
-      <Title
-        order={2}
-        size="h1"
-        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}` })}
-        weight={900}
-        align="center"
-      >
-        Send us a <span style={{
-            color: `rgb(0,208,132)`,
-        }}>message</span>
-      </Title>
-      <SimpleGrid cols={2} mt="xl" breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-        <TextInput
-          label="Name"
-          placeholder="Your name"
-          name="name"
-          variant="filled"
-          {...form.getInputProps('name')}
-        />
-        <TextInput
-          label="Email"
-          placeholder="Your email"
-          name="email"
-          variant="filled"
-          {...form.getInputProps('email')}
-        />
-      </SimpleGrid>
-
+    <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '10px' }}>
+  <form onSubmit={form.onSubmit(() => {})}>
+    <Title
+      order={2}
+      size="h1"
+      sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}` })}
+      weight={900}
+      align="center"
+    >
+      Send us a <span style={{ color: 'rgb(0,208,132)' }}>message</span>
+    </Title>
+    <SimpleGrid cols={2} mt="xl" breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
       <TextInput
-        label="Subject"
-        placeholder="Subject"
-        mt="md"
-        name="subject"
+        label="Name"
+        placeholder="Your name"
+        name="name"
         variant="filled"
-        {...form.getInputProps('subject')}
+        {...form.getInputProps('name')}
       />
-      <Textarea
-        mt="md"
-        label="Message"
-        placeholder="Your message"
-        maxRows={10}
-        minRows={5}
-        autosize
-        name="message"
+      <TextInput
+        label="Email"
+        placeholder="Your email"
+        name="email"
         variant="filled"
-        {...form.getInputProps('message')}
+        {...form.getInputProps('email')}
       />
+    </SimpleGrid>
 
-      <Group position="center" mt="xl">
-        <Button type="submit" size="md" style={{
-        backgroundColor: `rgb(0, 208, 132)`,
-        borderRadius: '30px',
-            }}>
+    <TextInput
+      label="Subject"
+      placeholder="Subject"
+      mt="md"
+      name="subject"
+      variant="filled"
+      {...form.getInputProps('subject')}
+    />
+    <Textarea
+      mt="md"
+      label="Message"
+      placeholder="Your message"
+      maxRows={10}
+      minRows={5}
+      autosize
+      name="message"
+      variant="filled"
+      {...form.getInputProps('message')}
+    />
+
+    <Group position="center" mt="xl">
+      <Button type="submit" size="md" style={{ backgroundColor: 'rgb(0, 208, 132)', borderRadius: '30px' }}>
         Send message
-        </Button>
-      </Group>
-    </form>
+      </Button>
+    </Group>
+  </form>
+</div>
   );
 }
