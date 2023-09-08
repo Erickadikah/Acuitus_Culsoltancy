1e5f6c437a29a4ac03b1b287e3b0e85767f8a80c
erickadikah <erickadikah2030@gmail.com>
2023-09-08 12:23:30 +0300

    update

--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const messageRoutes = express.Router();
 const Message = require('../models/request');
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+
+
+const transporter = nodemailer.createTransport({
+  service: 'hotmail', // Use the Gmail service
+  auth: {
+    user: 'outlook_8F87BF6D0AD80771@outlook.com', // Replace with your Gmail email address
+    pass: 'adikah2030', // Replace with your Gmail password or generate an app-specific password
+  },
+});
 
 messageRoutes.post('/message', async (req, res) => {
+  console.log('Received POST request');
   try {
     let message = new Message({
       name: req.body.name,
@@ -11,9 +23,18 @@ messageRoutes.post('/message', async (req, res) => {
       message: req.body.message
     });
     await message.save();
+
+    const mailOptions = {
+      from: 'outlook_8F87BF6D0AD80771@outlook.com',
+      to: 'erickadikah2030@gmail.com',
+      subject: 'New Message Received',
+      text: `You have received a new message from ${message.name} (${message.email}).\n\nSubject: ${message.subject}\n\nMessage: ${message.message}`,
+    };
+    await transporter.sendMail(mailOptions);
+
     res.status(200).json({ message: 'Message saved successfully', message });
   } catch (error) {
-    // Handle any errors
+    console.error('Error:', error); // Add this line
     res.status(500).json({ message: error.toString() });
   }
 });
